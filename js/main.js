506ab266c24e97c3ae6cf5bbff70f5a91991d203
Mjumbe Poe <mjumbewu@gmail.com>
2023-10-25 13:52:42 -0400

    Clear out the repo

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import { initializeMap } from './map.js';
-
-const stationInfoResp = await fetch('https://gbfs.bcycle.com/bcycle_indego/station_information.json');
-const stationInfo = await stationInfoResp.json();
-
-initializeMap(stationInfo);
