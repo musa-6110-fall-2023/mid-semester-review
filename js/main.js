93dc4ee15c559a81474e4456266246bab9a5f2f7
Mjumbe Poe <mjumbewu@gmail.com>
2023-10-24 23:32:48 -0400

    Refactor the map code into a separate module

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-const map = L.map('map').setView([39.95, -75.16], 12);
+import { initializeMap } from './map.js';
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}@2x?access_token={accessToken}', {
-  tileSize: 512,
-  maxZoom: 19,
-  zoomOffset: -1,
-  id: 'mapbox/streets-v12',
-  attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
-  accessToken: 'pk.eyJ1IjoibWp1bWJlLXRlc3QiLCJhIjoiY2wwb3BudmZ3MWdyMjNkbzM1c2NrMGQwbSJ9.2ATDPobUwpa7Ou5jsJOGYA',
-}).addTo(map);
+initializeMap();
